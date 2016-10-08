3d3dde42c97fb46b18260117850ba9c9fffcd70c
Jaiye Senbanjo <jaiye.s@gmail.com>
2016-10-08 17:38:22 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 
 <div class="row black_text">
 		
-		<div class="col-xs-12 col-sm-5 col-md-5 col-lg-5" style="padding-left: 2%;" id="careers">
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4" style="padding-left: 2%;" id="careers">
 			<h3 class="green_text">Contact Us</h3>
 			<table class="table no_und">
 			<tr><td>Verto Homes</td>
@@ -26,25 +26,11 @@
 			<a href="#menu">Read More</a>
 		</div>
 		
-		<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 text-center"  style="padding-left: 2%;">
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 text-center"  style="padding-left: 2%;">
 			<img src="images/awards.png" class="img-responsive img_center">
 		</div>
 		
 </div>
-
-<!--
-<div class="row">
-	<table class="table no_und" style="margin-left: 5%;">
-	<tr><td><h3 class="green_text">Contact Us</h3></td><td></td><td><h3 class="green_text">Latest Post</h3></td><td rowspan="5"><img src="images/awards.png" class="img-responsive img_center"></td><td>&nbsp;</td><td>&nbsp;</td></tr>
-	<tr><td>Verto Homes</td><td><span class="glyphicon glyphicon-earphone"></span>&nbsp;Tel +44(0)203 745 7374</td><td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</td><td></td><td>&nbsp;</td><td>&nbsp;</td></tr>
-	<tr><td>71 Queen Victoria Street</td><td><span class="glyphicon glyphicon-envelope"></span>&nbsp;Email info@vertohomes.com</td><td>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </td><td></td><td>&nbsp;</td><td>&nbsp;</td></tr>
-	<tr><td>London</td><td><img src="images/facebook.png" class="mouse_pointer">&nbsp;</a><img src="images/twitter.png" class="mouse_pointer">&nbsp;</a>
-				<img src="images/instagram.png" class="mouse_pointer"></a></td><td>exercitation ullamco laboris nisi ut aliquip.</td><td></td><td>&nbsp;</td><td>&nbsp;</td></tr>
-	<tr><td>EC4V 4AY</td><td></td><td><a href="#menu" class="a_green_link">Read More</a></td><td></td><td>&nbsp;</td><td>&nbsp;</td></tr>
-	</table>
-</div>
-<br>
--->
 
 <div class="row">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center black_text_2 green_link">
@@ -52,7 +38,6 @@
 		<p>Copyright &copy; 2015 Verto Homes, All Rights Reserved</p>
 	</div>	
 </div>
-
 
 
 </div class="container-fluid">	
@@ -64,7 +49,5 @@ $(window).load(function() {
 </script>
 
 </body>
-<!--
-<script src="js/ruin_js.js"></script>
--->
+
 </HTML>
